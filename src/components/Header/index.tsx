a5af27dec313ce21e8ad5dad9167a80acb6a7b61
shioncha <57836246+shioncha@users.noreply.github.com>
2024-09-11 21:12:52 +0900

    Add: dynamic display of navigation based on sign-in status - ログイン状態による動的なナビゲーションの切り替え - NavLinkを使用し、現在開いているページがわかるように - 装飾

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,22 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
 import { useContext } from 'react';
 import { AuthContext } from '@/context/auth';
 
 const Header = () => {
-  const { user } = useContext(AuthContext);
+  const { userData } = useContext(AuthContext);
   return (
     <header>
       <h1>Sample Apps</h1>
       <nav>
         <ul>
-          <li><Link to={'/'}>Top</Link></li>
-          <li><Link to={'/weather/'}>Weather</Link></li>
-          <li><Link to={'/dev/'}>Dev</Link></li>
-          { user ? <p>{user.email}</p> : 
+          <li><NavLink to={'/'}>Top</NavLink></li>
+          <li><NavLink to={'/weather/'}>Weather</NavLink></li>
+          <li><NavLink to={'/dev/'}>Dev</NavLink></li>
+          { userData.name ? <li><NavLink to={'/mypage/'}>{userData.name}</NavLink></li> : 
             <>
-              <li><Link to="/signin/">Sign in</Link></li>
-              <li><Link to="/signup/">Sign up</Link></li>
+              <li><NavLink to="/signin/">Sign in</NavLink></li>
+              <li><NavLink to="/signup/">Sign up</NavLink></li>
             </>
           }
         </ul>
